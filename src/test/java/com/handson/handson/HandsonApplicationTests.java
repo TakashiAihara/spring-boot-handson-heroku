4cac41782b2687c6a4e647dee320a5a447c50f7b
TakashiAihara <takashi.aihara.0523@gmail.com>
2021-04-14 02:33:57 +0900

    base app

--- src/test/java/com/handson/handson/HandsonApplicationTests.java
+++ src/test/java/com/handson/handson/HandsonApplicationTests.java
@@ -1,10 +1,10 @@
-package com.handson.demo;
+package com.handson.handson;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class HandsonApplicationTests {
 
 	@Test
 	void contextLoads() {
