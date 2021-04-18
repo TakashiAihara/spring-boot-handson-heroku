4db40b9d98be420ea6a92819fed924748491a719
TakashiAihara <takashi.aihara.0523@gmail.com>
2021-04-18 15:23:45 +0900

    変更中

--- src/main/java/com/handson/handson/controller/UsersController.java
+++ src/main/java/com/handson/handson/controller/UsersController.java
@@ -1,10 +1,10 @@
 package com.handson.handson.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class IndexController {
+@RestController
+public class UsersController {
 
 
 	@GetMapping("/")
